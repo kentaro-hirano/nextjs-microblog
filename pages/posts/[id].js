654decde506b70c7fd4e53ec8e4be62309323f45
kentaro-hirano <yakyukentaro@gmail.com>
2024-01-10 23:49:49 +0900

    add: getStaticPathとgetStaticPropsの追加

---
+++ pages/posts/[id].js
@@ -0,0 +1,17 @@
+import Layout from "../../components/Layout";
+import { getAllPostIds } from "../../lib/post";
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {}
+
+export default function Post() {
+  return <Layout>動的ルーティング設定</Layout>;
+}
